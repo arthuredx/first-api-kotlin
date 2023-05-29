aa793ebd73778da5bec9f332c12b32c3d292c360
arthuredx <arthuredx@gmail.com>
2023-05-29 20:33:45 -0300

    first commit

---
+++ src/main/kotlin/com/coder/firstapikotlin/FirstApiKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.coder.firstapikotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstApiKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<FirstApiKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/coder/firstapikotlin/FirstApiKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coder.firstapikotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FirstApiKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
