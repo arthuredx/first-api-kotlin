c21a18bc5d8f5227d2fc17f0bedf56615a9d9ff4
arthuredx <arthuredx@gmail.com>
2023-05-31 21:54:44 -0300

    add tests

--- src/main/kotlin/com/coder/firstapikotlin/AccountController.kt
+++ src/main/kotlin/com/coder/firstapikotlin/AccountController.kt
@@ -2,22 +2,16 @@ package com.coder.firstapikotlin
 
 import io.micrometer.core.ipc.http.HttpSender.Response
 import jakarta.persistence.Id
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/accounts")
 class AccountController (private val repository: AccountRepository){
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun create (@RequestBody account: Account): Account = repository.save(account)
 
     @GetMapping
---
+++ src/test/kotlin/com/coder/firstapikotlin/AccountControllerTest.kt
@@ -0,0 +1,96 @@
+package com.coder.firstapikotlin
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+    @Autowired lateinit var mockMvc: MockMvc
+
+    @Autowired lateinit var accountRepository: AccountRepository
+
+    @Test
+    fun `test find all`(){
+        accountRepository.save(Account(name = "Test", document = "123", phone = "61 9812"))
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/accounts"))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$").isArray) // se alguem trocar esse tipo de dados, o teste vai acusar, isso serve pra garantir que o contrato esta sendo mantido.
+            .andExpect(MockMvcResultMatchers.jsonPath("\$[0].id").isNumber)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$[0].name").isString)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$[0].document").isString)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$[0].phone").isString)
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+    @Test
+    fun `test find by id`(){
+        val account = accountRepository.save(Account(name = "Test", document = "123", phone = "61 9812"))
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/accounts/${account.id}"))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.id").value(account.id))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.name").value(account.name))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.document").value(account.document))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.phone").value(account.phone))
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+    @Test
+    fun `test create`(){
+        val account = Account(name = "Test 2", document = "222", phone = "61 9812")
+        val accountJson = ObjectMapper().writeValueAsString(account)
+        accountRepository.deleteAll()
+        mockMvc.perform(MockMvcRequestBuilders.post("/accounts")
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(accountJson))
+            .andExpect(MockMvcResultMatchers.status().isCreated)
+            .andDo(MockMvcResultHandlers.print())
+
+        Assertions.assertFalse(accountRepository.findAll().isEmpty())
+    }
+
+    @Test
+    fun `test update`(){
+        val account = accountRepository.
+            save(Account(name = "Test", document = "123", phone = "61 9812"))
+            .copy(name="Update")
+        val accountJson = ObjectMapper().writeValueAsString(account)
+        mockMvc.perform(MockMvcRequestBuilders.put("/accounts/${account.id}")
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(accountJson))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andDo(MockMvcResultHandlers.print())
+
+        val findById = accountRepository.findById(account.id!!.toLong())
+        Assertions.assertTrue(findById.isPresent)
+        Assertions.assertEquals(account.name, findById.get().name)
+    }
+
+    @Test
+    fun `test delete`(){
+        val account = accountRepository.
+        save(Account(name = "Test", document = "123", phone = "61 9812"))
+        mockMvc.perform(MockMvcRequestBuilders.delete("/accounts/${account.id}"))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andDo(MockMvcResultHandlers.print())
+
+        val findById = accountRepository.findById(account.id!!.toLong())
+        Assertions.assertFalse(findById.isPresent)
+    }
+}
